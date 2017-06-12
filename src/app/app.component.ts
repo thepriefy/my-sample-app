5eb889f997e9aa1e603d6797400ad0505a1842e5
Priefy <peachy-lovly-mom@hotmail.com>
2017-06-13 00:47:19 +0700

    Add&Delete comment from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable  } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments: {name: string, text: string}[] = [
-    {name: 'testUser', text: 'Example text'}
-  ];
+  // comments: {name: string, text: string}[] = [
+  //   {name: 'testUser', text: 'Example text'}
+  // ];
 
   userName = '';
   userText = '';
 
-  onPostMsg() {
-    this.comments.push({name: this.userName, text: this.userText});
-    this.userName = '';
-    this.userText = '';
+  comments: FirebaseListObservable<any>;
+
+  constructor(db: AngularFireDatabase) {
+    this.comments = db.list('/items');
   }
+
+  // onPostMsg() {
+  //   this.comments.push({name: this.userName, text: this.userText});
+  //   this.userName = '';
+  //   this.userText = '';
+  // }
+
+  addItem() {
+    // this.items.push({ text: newName });
+    this.comments.push({name: this.userName, text: this.userText});
+  }
+  // updateItem(key: string, newText: string) {
+  //   this.comments.update(key, { text: newText });
+  // }
+  deleteItem(key: string) {
+    console.log(key);
+    this.comments.remove(key);
+  }
+  // deleteEverything() {
+  //   this.comments.remove();
+  // }
 }
